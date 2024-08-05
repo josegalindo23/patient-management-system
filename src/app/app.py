9ce9dc146474832cd7c7f8b06ecd5b86956f776c
Jose Galindo <juniorgala23@gmail.com>
2024-08-05 02:30:40 -0500

    desactivate debug

--- src/app/app.py
+++ src/app/app.py
@@ -2,4 +2,4 @@ from src.infrastructure.controller import api_rest
 
 if __name__ == "__main__":
     api_rest.app.register_error_handler(404,api_rest.page_not_found)
-    api_rest.app.run(host="0.0.0.0", port=8080, debug=True)
+    api_rest.app.run(host="0.0.0.0", port=8080, debug=False)
